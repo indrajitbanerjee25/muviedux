80672a4e0f5810720105bcb53f0608b5ebd369f9
Indrajit Banerjee <indrajit.banerjee25@gmail.com>
2026-02-16 12:10:31 +0530

    Initial commit

---
+++ src/components/MovieCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles.css';
+
+const MovieCard = ({ movie }) => {
+  const handleError = (e) => {
+    e.target.src = '/images/default.jpg';
+  };
+  return (
+    <div key={movie.id} className='movie-card'>
+      <img
+        src={`images/${movie.image}`}
+        alt={movie.title}
+        onError={handleError}
+      />
+      <div className='movie-card-info'>
+        <h3 className='movie-card-title'>{movie.title}</h3>
+        <p className='movie-card-genre'>{movie.genre}</p>
+        <p className='movie-card-rating'>{movie.rating}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
