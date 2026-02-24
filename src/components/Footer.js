c8f806747d4a84b609b3f019dd491d6a5cc8383d
Indrajit Banerjee <indrajit.banerjee25@gmail.com>
2026-02-24 10:10:10 +0530

    Add para

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,11 @@ export default function Footer() {
     <footer className='footer'>
       <p className='footer-text'>
         © {currentYear} Moviedux, All right reserved.
+        <p className='text-xs bg text-center mt-6'>
+          This is a personal learning project created for educational purposes
+          only. It is not affiliated with, endorsed by, or any other company or
+          service.
+        </p>
       </p>
     </footer>
   );
